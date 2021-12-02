9ab903beb24e7c6abfbee47f34aecd8501c6b9d4
emmanuelraditya <86480069+emmanuelraditya@users.noreply.github.com>
2021-12-03 00:17:31 +0700

    first commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+interface Items {
+  name?: string;
+  type?: string;
+  prices?: number;
+  priceFor?: string;
+}
+
+const items:Items = {
+    name: "oval hat",
+    type: "hats",
+    pricesFor: "regular",
+    prices:30000
+}
